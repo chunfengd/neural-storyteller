15a4fb9e9f270a54923615ae24e135a682841b27
chunfeng <chunfeng@huitu>
2016-03-25 14:19:59 -0400

    add config.init

--- config.py
+++ config.py
@@ -36,3 +36,24 @@ paths['captions'] = '/ais/gobi3/u/rkiros/storyteller/coco_train_caps.txt'
 # Biases
 paths['negbias'] = '/ais/gobi3/u/rkiros/storyteller/caption_style.npy'
 paths['posbias'] = '/ais/gobi3/u/rkiros/storyteller/romance_style.npy'
+
+def init(model_cache_path):
+    global paths
+    # skip-thoughts
+    paths['skmodels'] = '{}/skip-thoughts/'.format(path)
+    paths['sktables'] = '{}/skip-thoughts/'.format(path)
+    # decoder
+    paths['decmodel'] = '{}/romance.npz'.format(path)
+    paths['dictionary'] = '{}/romance_dictionary.pkl'.format(path)
+    # Image-sentence embedding
+    paths['vsemodel'] = '{}/coco_embedding.npz'.format(path)
+    # VGG-19 convnet
+    paths['vgg'] = '{}/vgg19.pkl'.format(path)
+    paths['pycaffe'] = '/u/yukun/Projects/caffe-run/python'
+    paths['vgg_proto_caffe'] = '{}/VGG_ILSVRC_19_layers_deploy.prototxt'.format(path)
+    paths['vgg_model_caffe'] = '{}/VGG_ILSVRC_19_layers.caffemodel'.format(path)
+    # COCO training captions
+    paths['captions'] = '{}/coco_train_caps.txt'.format(path)
+    # Biases
+    paths['negbias'] = '{}/caption_style.npy'.format(path)
+    paths['posbias'] = '{}/romance_style.npy'.format(paht)
